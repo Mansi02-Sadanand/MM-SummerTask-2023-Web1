e5b2205a08b14d0e705a5251680101805e83c706
Mansi02-Sadanand <129657030+Mansi02-Sadanand@users.noreply.github.com>
2023-05-23 00:41:41 +0530

    First Commit

---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@mui/material";
+export const MuiTypography =() =>{
+    return(
+        <div>
+            <Typography variant='h1'>Heading</Typography>
+            <Typography variant='h2'>Heading</Typography>
+            <Typography variant='h3'>Heading</Typography>
+            <Typography variant='h4'>Heading</Typography>
+            <Typography variant='h5'>Heading</Typography>
+
+        </div>
+    )
+}
